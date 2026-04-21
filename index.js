a60917403b1fea94e0736804ca8a47bfb565fc7f
nicohaines11 <nicohaines11@gmail.com>
2026-04-20 21:56:17 -0400

    Reorganizing files for setting up server

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const userRoutes = require("./server/routes/user")
+app.use("/users", userRoutes)
+
+const PORT = process.env.PORT || 3500
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!!`))
